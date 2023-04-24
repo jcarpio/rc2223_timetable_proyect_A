02faef0016702e364a4a2e25a91f6443a07fc247
sergious234 <sergious234@gmail.com>
2023-04-24 09:36:31 +0200

    Add atributes of Asignaturas

--- parser/main.py
+++ parser/main.py
@@ -6,38 +6,52 @@ import html
 
 URL = "http://apppruebaetsi.uhu.es/simplesaml/app_gestion_cursos/Teoria/xml/plantilla_xml_horarios_2.php?tit=G26&year=2021&cuatr=1"
 FICHERO = "Datos.txt"
-class Asignatura:
-    codigo:         str
-    nombre:         str
-    titulacion:     str
-    itinerario:     str
-    curso:          str
-    nombreingles:   str
-    tipo:           str
+ASIGNATURA_TAG = "<Asignatura "
 
-    def __new__(self, line: str):
+
+class ATRIBUTES:
+    CODIGO: str = "codigo"
+    NOMBRE: str = "nombre"
+    TITULACION: str = "titulacion"
+    ITINERARIO: str = "itinerario"
+    CURSO: str = "curso"
+    NOMBREINGLES: str = "nombreingles"
+    TIPO: str = "tipo"
+
+
+class Asignatura:
+    atributos: dict[str, str]
+
+    def __init__(self, line: str):
         line = line[1:len(line)-2]
-        atributes: list[str] = line.strip("<Asignatura ").split("\" ")
+        atributes: list[str] = line.strip(ASIGNATURA_TAG).split("\" ")
+        self.atributos = {}
                 
         with open(FICHERO, "a") as file:
             for a in atributes:
                 data = a.strip().split("=\"")
-                data: str = f"{data[0]}  {html.unescape(data[1])}\n"
+                data[1] = html.unescape(data[1])
+                self.atributos[data[0]] = data[1]
+
+                data: str = f"{data[0]}  {data[1]}\n"
                 file.write(data)
             file.write("\n\n")
 
 def main():
     response = requests.get(URL)
     data = response.text
+    asignaturas: list[Asignatura] = []
 
     if os.path.exists(FICHERO):
         f = open(FICHERO, "w")
         f.close()
 
     for line in data.split("\n"):
-        if "<Asignatura" in line:
-            a = Asignatura(line)
+        if ASIGNATURA_TAG in line:
+            asignaturas.append(Asignatura(line))
 
+    primero = [x.atributos.get(ATRIBUTES.NOMBRE) for x in asignaturas if x.atributos.get(ATRIBUTES.CURSO) == "1"]
+    print(f"{primero}")
 
 
 if __name__ =="__main__":
