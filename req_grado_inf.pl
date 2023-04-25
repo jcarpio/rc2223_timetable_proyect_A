6b6c05354d21496a1447bfea84d95649b05cec9d
jcarpio <jose.carpio@gmail.com>
2023-04-25 14:12:11 +0200

    Update req_grado_inf.pl

--- req_grado_inf.pl
+++ req_grado_inf.pl
@@ -2,4 +2,6 @@ slots_per_week(40).
 
 slots_per_day(8).
 
-class_subject_teacher_times('GraIngInf1t1', 'Fundamentos de Computadores', fundcompt1, 2).
+class_subject_teacher_times('Inf1t1', 'Fundamentos de Computadores', fundcompInfT1, 2).
+class_subject_teacher_times('Inf1t1', 'Fisica', fisicaInf1T1, 2).
+
