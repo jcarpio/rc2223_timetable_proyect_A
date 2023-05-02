d52e8f3de839a3992629ffd441d2cfcc245f405b
Israel <israelaznarvillegas@gmail.com>
2023-05-02 16:51:59 +0200

    Asignaturas hasta 3

--- req_inf.pl
+++ req_inf.pl
@@ -20,6 +20,59 @@ class_subject_teacher_times('Inf1T3', 'Fundamentos de Programacion', fundProgInf
 class_subject_teacher_times('Inf1t3', 'Administración Y Economía de la Empresa', admEmpInf1T3, 2).
 class_subject_teacher_times('Inf1t3', 'Matematicas 1', matIinf1T3, 2).
 
+class_subject_teacher_times('Inf1T1', 'Tecnología de Computadores', tecncompInf1T1, 2).
+class_subject_teacher_times('Inf1T1', 'MatemáticasII', matIIInf1T1, 2).
+class_subject_teacher_times('Inf1T1', 'MatemáticasIII', matIIIInf1T1, 2).
+class_subject_teacher_times('Inf1T1', 'Fundamentos de Análisis de Algoritmos', fundanaalgInf1T1, 2).
+class_subject_teacher_times('Inf1T1', 'Estructura de Datos I', tecncompInf1T1, 2).
+
+class_subject_teacher_times('Inf1T2', 'Tecnología de Computadores', tecncompInf1T2, 2).
+class_subject_teacher_times('Inf1T2', 'MatemáticasII', matIIInf1T2, 2).
+class_subject_teacher_times('Inf1T2', 'MatemáticasIII', matIIIInf1T2, 2).
+class_subject_teacher_times('Inf1T2', 'Fundamentos de Análisis de Algoritmos', fundanaalgInf1T2, 2).
+class_subject_teacher_times('Inf1T2', 'Estructura de Datos I', tecncompInf1T2, 2).
+
+class_subject_teacher_times('Inf1T3', 'Tecnología de Computadores', tecncompInf1T3, 2).
+class_subject_teacher_times('Inf1T3', 'MatemáticasII', matIIInf1T3, 2).
+class_subject_teacher_times('Inf1T3', 'MatemáticasIII', matIIIInf1T3, 2).
+class_subject_teacher_times('Inf1T3', 'Fundamentos de Análisis de Algoritmos', fundanaalgInf1T3, 2).
+class_subject_teacher_times('Inf1T3', 'Estructura de Datos I', tecncompInf1T3, 2).
+
+
+
+class_subject_teacher_times('Inf2T1', 'Fundamentos de Redes de Computadores', fundredcompInf2T1, 2).
+class_subject_teacher_times('Inf2T1', 'Bases de datos', basdatInf1T1, 2).
+class_subject_teacher_times('Inf2T1', 'Estructura de Computadores', estcmpInf1T1, 2).
+class_subject_teacher_times('Inf2T1', 'Diseño y Estructura de los Sistemas Operativos', disestrsoInf1T1, 2).
+class_subject_teacher_times('Inf2T1', 'Metodología de la Programación', metprogInf1T1, 2).
+
+class_subject_teacher_times('Inf2T2', 'Fundamentos de Redes de Computadores', fundredcompInf2T2, 2).
+class_subject_teacher_times('Inf2T2', 'Bases de datos', basdatInf2T2, 2).
+class_subject_teacher_times('Inf2T2', 'Estructura de Computadores', estcmpInf2T2, 2).
+class_subject_teacher_times('Inf2T2', 'Diseño y Estructura de los Sistemas Operativos', disestrsoInf2T2, 2).
+class_subject_teacher_times('Inf2T2', 'Metodología de la Programación', metprogInf2T2, 2).
+
+class_subject_teacher_times('Inf2T1', 'Arquitectura de Computadores', arqcompInf2T1, 2).
+class_subject_teacher_times('Inf2T1', 'Principios y Fundamentos de la Ingeniería del Software', prifuningsoInf2T1, 2).
+class_subject_teacher_times('Inf2T1', 'Estructura de Datos II', estdatInf2T1, 2).
+class_subject_teacher_times('Inf2T1', 'Inteligencia Artificial', intartInf2T1, 2).
+class_subject_teacher_times('Inf2T1', 'Administración y Programa de Sistemas Operativos', adprosoInf2T1, 2).
+
+
+class_subject_teacher_times('Inf2T2', 'Arquitectura de Computadores', arqcompInf2T2, 2).
+class_subject_teacher_times('Inf2T2', 'Principios y Fundamentos de la Ingeniería del Software', prifuningsoInf2T2, 2).
+class_subject_teacher_times('Inf2T2', 'Estructura de Datos II', estdatInf2T2, 2).
+class_subject_teacher_times('Inf2T2', 'Inteligencia Artificial', intartInf2T2, 2).
+class_subject_teacher_times('Inf2T2', 'Administración y Programa de Sistemas Operativos', adprosoInf2T2, 2).
+
+
+class_subject_teacher_times('Inf3T1', 'Elaboración de Proyectos Informáticos', elaproinfInf3T1, 2).
+class_subject_teacher_times('Inf3T1', 'Diseño y Desarrollo de Sistemas de Información', disdessisinfInf3T1, 2).
+class_subject_teacher_times('Inf3T1', 'Algorítmica y Modelos de Computación', algmodcmpInf3T1, 2).
+class_subject_teacher_times('Inf3T1', 'Programación Concurrente y Distribuida', procondisInf3T1, 2).
+class_subject_teacher_times('Inf3T1', 'Interconexión de Redes de Computadores', intredcmpInf3T1, 2).
+
+
 % subject_coupling('Inf1t1', 'Matematicas I', 'Matematics 1').
 
 % morning_turn('Inf1T1').
